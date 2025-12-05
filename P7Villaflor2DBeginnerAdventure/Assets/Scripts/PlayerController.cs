1b481e770c07ea1322b15b67ba5cb2d2bd4b6ffe
D1luffy <723105@student.dusd.net>
2025-12-05 15:29:59 -0800

    added coliders
    I added coliders to the tile map and to objects

--- P7Villaflor2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7Villaflor2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -4,30 +4,50 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-    
+    // Variables related to player character movement
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public float speed = 3.0f;
+
+    // Variables related to the health system
+    public int maxHealth = 5;
+    int currentHealth;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-
-
     }
-       void FixedUpdate() 
+
+
+    void FixedUpdate()
     {
         Vector2 position = (Vector2)rigidbody2d.position + move * 6.0f * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
+
+
+    void ChangeHealth (int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+
+
+
+
+
+
 }
